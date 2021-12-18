7fc098361967e42d619a489d269215a9662d178b
WhoDatBoi <bohdan.butenko@nure.ua>
2021-12-18 13:27:45 +0200

    New AnnouncementDates entity was added + some changes for Announcements entity

---
+++ Tutor.DAL/Entities/AnnouncementDates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tutor.DAL.Entities
+{
+    public class AnnouncementDates
+    {
+        public int Id { get; set; }
+
+        public int AnnouncementId { get; set; }
+        public Announcements Announcement { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int DayOfWeek { get; set; }
+    }
+}
